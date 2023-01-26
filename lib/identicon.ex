35f5ae5768a9bd61dc60fc7fc8c70c75038d42ab
Patrick McCabe <pmccabe.dev@gmail.com>
2023-01-26 18:04:02 +0000

    updates

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -19,6 +19,39 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image( input)
+  end
+
+   @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identicon.hello()
+      :world
+
+  """
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+    @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identicon.hello()
+      :world
+
+  """
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+    :egd.render(image)
   end
 
   @doc """
@@ -31,7 +64,16 @@ defmodule Identicon do
 
   """
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = rem(index, 5) * 50
 
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
 
